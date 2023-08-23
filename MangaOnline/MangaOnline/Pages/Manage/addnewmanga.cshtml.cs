ad94f2d69416894607f82fd7d7b5091767877492
lelong181 <longld2284@gmail.com>
2023-08-23 23:11:52 +0700

    Add new manga
    Add new manga

--- MangaOnline/MangaOnline/Pages/Manage/addnewmanga.cshtml.cs
+++ MangaOnline/MangaOnline/Pages/Manage/addnewmanga.cshtml.cs
@@ -1,3 +1,4 @@
+using MangaOnline.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace MangaOnline.Pages.Manage
 {
     public class addnewmangaModel : PageModel
     {
+        private readonly MangaOnlineV1DevPRN221Context _context;
+
+        public List<Author> authors { get; set; }
+        public List<Category> categories { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public addnewmangaModel(MangaOnlineV1DevPRN221Context context)
+        { 
+            _context = context;
+            categories = _context.Categories.ToList();
+            authors = _context.Authors.ToList();
+        }
         public void OnGet()
         {
+            //authors = _context.Authors.ToList();
+            //categories = _context.Categories.ToList();
         }
     }
 }
